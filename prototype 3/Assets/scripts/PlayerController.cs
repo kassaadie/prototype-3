d86f4cdded96697bc723c3e264f1dca6aa817e71
kassadie barney <kassadie.b161@stu.nebo.edu>
2019-11-07 09:31:26 -0700

    stage

---
+++ prototype 3/Assets/scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{ private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plyerRb
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
