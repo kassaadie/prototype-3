bb6b83f8a520c1a3c4cd05e6e88c68f6c5b07772
kassadie barney <kassadie.b161@stu.nebo.edu>
2019-11-19 10:29:19 -0700

    3.2

--- prototype 3/Assets/scripts/PlayerController.cs
+++ prototype 3/Assets/scripts/PlayerController.cs
@@ -3,27 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
-{ private Rigidbody playerRb;
-    public float jumpForce;
+{   private Rigidbody playerRb;
+    private Animator playerAnim;
+    public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
-    private Animtor playerAnim;
+    public bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
-        Physics.gravity *= gravityModifier;}
+        Physics.gravity *= gravityModifier;
     }
 
     void Update() {
 
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gamover) { 
-        playerRb.AddForce(Vector3.up * 10 jumpForce, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver != true ) { 
+        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         isOnGround = false; } }
-        playerAnim.SetTrigger("Jump_trig"):}}
-    }    
+        playerAnim.SetTrigger("Jump_trig"); } 
+    }
 
-     private void OnCollisionEnter(Collision collision) {
-      isOnGround = true; }
+private void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.CompareTag("Ground"))
+    {
+        isOnGround = true;
+    }
+    else if (collision.gameObject.CompareTag("Obstacle"))
+    {
+        gameOver = true;
+        Debug.Log("Game Over!");
+        playerAnim.SetBool("Death_b", true);
+        playerAnim.SetInteger("DeathType_int", 1); } }
+
+    }
+}
