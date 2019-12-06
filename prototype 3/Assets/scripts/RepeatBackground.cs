d809a10666e1ddc8aa457f7bddea824e778086cd
kassadie barney <kassadie.b161@stu.nebo.edu>
2019-12-06 10:03:05 -0700

    add challge

--- prototype 3/Assets/scripts/RepeatBackground.cs
+++ prototype 3/Assets/scripts/RepeatBackground.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
-{  private Vector3 statPos;
-    private float repeatWidth; 
+{ private Vector3 statPos;
+  private float repeatWidth; 
     // Start is called before the first frame update
     void Start(){
     {   
-      statPos = transform.psition; }
+      statPos = transform.position; }
       repeatWidth = GetComponent<BoxCollider>().size.x / 2;}
 
-    // Update is called once per frame
+    // Update is called once per frame 
     void Update()
     {
-        if (transform.position.x < startPos.x -50) {
+        if (transform.position.x < startPos.x -repeatWidth) {
           transform.postion = startPos; } }
     }
 }
